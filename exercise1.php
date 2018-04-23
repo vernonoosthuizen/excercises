2ae1e3f2d9e041176ce006f77f540a22915bd3ef
Vernon Oosthuizen <vernonoosthuizen@stratusolve.com>
2018-04-23 12:43:19 +0200

    testing commit

---
+++ exercise1.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vernon Oosthuizen
+ * Date: 2018/04/23
+ * Time: 12:38 PM
+ */
+
+function addAll($Array){
+    print_r($Array);
+    return '';
+}
+
+$Array = [1,1,1,1,1];
+echo addAll($Array);
