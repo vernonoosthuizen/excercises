1914723b3daffc18a34aeadc4d23261b79c48d3e
Vernon <vernon.oosthuizen@stratusolve.com>
2018-05-02 09:02:12 +0200

    committing exercise1 with loop

--- exercise1.php
+++ exercise1.php
@@ -2,14 +2,21 @@
 /**
  * Created by PhpStorm.
  * User: Vernon Oosthuizen
- * Date: 2018/04/23
- * Time: 12:38 PM
+ * Date: 2018/05/02
+ * Time: 8:23 AM
  */
 
-function addAll($Array){
-    print_r($Array);
-    return '';
+function addAll($Array) {
+    $returnValue = 0;
+    while (count($Array) > 0) {
+        foreach ($Array as $key=>$value) $returnValue += $value;
+        array_shift($Array);
+    }
+    return $returnValue;
 }
 
-$Array = [1,1,1,1,1];
+$Array = [1,1,1,1,1]; //5+4+3+2+1=15
+
 echo addAll($Array);
+
+?>
